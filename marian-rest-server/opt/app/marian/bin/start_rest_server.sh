a27587a893285abe9b84b13d1350d05829d9b2ad
Ulrich Germann <ugermann@inf.ed.ac.uk>
2020-07-22 12:15:53 +0100

    Pay attention to NPROC in start_rest_server.sh

--- marian-rest-server/opt/app/marian/bin/start_rest_server.sh
+++ marian-rest-server/opt/app/marian/bin/start_rest_server.sh
@@ -3,7 +3,7 @@
 NPROC=${NPROC:-$(nproc)}
 
 if [[ "$(which nvidia-smi)" == "" ]]; then
-    bin/rest-server --cpu-threads $(nproc) $@
+    bin/rest-server --cpu-threads $NPROC $@
 else
     bin/rest-server $@
 fi
